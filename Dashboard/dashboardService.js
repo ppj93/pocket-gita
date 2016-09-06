812b5cf382020f00d20ade33117bb1c3db942772
ppj93 <pravinjoshi95@gmail.com>
2016-09-06 09:26:41 +0530

    basic ui to service flow working

--- Dashboard/dashboardService.js
+++ Dashboard/dashboardService.js
@@ -1,16 +1,16 @@
 var express = require('express'),
-    config = require('./config'),
+    config = require('./dashboardConfig'),
     fs = require('fs'),
-    handlebars = require('express3-handlebars'),
+    handlebars = require('express-handlebars'),
     app = express();
 
 app.set('port', config.appConfig.port);
 
 app.use(express.static(__dirname + '/public'));
 
-app.engine('handlebars', handlebars.engine);
+app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
-
+app.set('views', __dirname + '/public/views')
 app.get('/', function (req, res) {
     res.render('index');
 });
@@ -24,10 +24,16 @@ app.use(function(req,res,next){
     if(autoViews[path]) return res.render(autoViews[path]);
     // if it's not in the cache, see if there's
     // a .handlebars file that matches
-    if(fs.existsSync(__dirname + '/views/admin' + path + '.handlebars')){
+    if(fs.existsSync(__dirname + '/public/views' + path + '.handlebars')){
         autoViews[path] = path.replace(/^\//, '');
         return res.render(autoViews[path]);
     }
     // no view found; pass on to 404 handler
     next();
+});
+
+app.listen(config.appConfig.port, function () {
+    console.log("Dashboard listening on port: " +
+        config.appConfig.port
+    );
 });
